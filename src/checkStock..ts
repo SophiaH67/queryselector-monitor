6a8f80920b74c978d16b2e55efff880f1e346a8a
Sophia Hage <sophia@sophiah.gay>
2023-11-25 18:07:13 +0100

    Fix puppeteer launch options and idle wait

--- src/checkStock..ts
+++ src/checkStock..ts
@@ -5,15 +5,23 @@ export async function checkStock(
   querySelector: string,
   timeout = 30_000
 ): Promise<boolean> {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: "new",
+  });
   const page = await browser.newPage();
   await page.goto(url);
 
   // Wait for page to load
-  const element = await page.waitForSelector(querySelector, {
-    visible: true,
-    timeout,
-  });
+  await page
+    .waitForNetworkIdle({
+      idleTime: 5_000,
+      timeout,
+    })
+    .catch(() => {
+      console.warn(`Timed out waiting for ${url} to load.`);
+    });
+
+  const element = await page.$(querySelector);
 
   const wasFound = !!element;
 
